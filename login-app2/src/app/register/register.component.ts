440863b367b63aa6a45830768669d6f150725c6a
ShawnelleMcNichols <Shawnelle.McNichols@WeAreHGC.com>
2024-10-11 14:58:05 -0400

    Login-app2

--- login-app2/src/app/register/register.component.ts
+++ login-app2/src/app/register/register.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-register',
   standalone: true,
